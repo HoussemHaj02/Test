183c1b20a46d68bd8d58175d7490e1eb57bd14b7
Houssem Hajji <houssemhajji4@gmail.com>
2024-05-21 15:51:10 +0100

    mise a jour

--- cypress/e2e/ConnexionAmazon.cy.js
+++ cypress/e2e/ConnexionAmazon.cy.js
@@ -1,45 +1,68 @@
 describe('My First Test: Connexion Amazon', () => {  
-  it('recherche sur un Produit', () => {    
+  it('recherche sur un Produit', () => {  
+
+    // Effacer tous les cookies  
     cy.clearAllCookies()
+
+    // Effacer tout le stockage local
     cy.clearAllLocalStorage()
-      cy.visit('https://www.amazon.com/')
-      //cy.wait(5000)
-      cy.get('#twotabsearchtextbox').type('Acer{enter}')
-     // cy.wait(5000)
-      cy.get('#a-autoid-1-announce').click()
-      //cy.wait(5000)
-      cy.get('#nav-cart').click()
-      cy.get('#a-autoid-0-announce').click()
-      cy.get('#ap_email').type('houssemhajji414@gmail.com')
-      cy.get('.a-button-inner > #continue').click()
-      cy.get('#ap_password').type('azerTY')
-      cy.get('#signInSubmit').click()
-      //cy.get('#ap-account-fixup-phone-skip-link').click()
-      cy.get('#sc-buy-box-ptc-button > .a-button-inner > .a-button-input').click()
-    //  cy.get('#a-autoid-1-announce > .a-size-base-plus').click()
-     // cy.get('#ap_customer_name').type('Houssem Hajji')
-    //  cy.get('#ap_email').type('houssemhajji414@gmail.com')
-     // cy.get('#ap_password').type('1025azerTY?')
-     // cy.get('#ap_password_check').type('1025azerTY?')
-     // cy.get('#continue').click()
-      //cy.get('#a-autoid-0-announce').click()
-   //cy.get('#ap_email').type('houssemhajji4@gmail.com')
-   //cy.get('.a-button-inner > #continue').click()
-  // cy.get('#continue').click()
-   /*cy.get('#cvf-input-code').type('656299')
-   cy.get('#cvf-submit-otp-button > .a-button-inner > .a-button-input').click()
-   cy.get('.a-input-text').type('Houssem Hajji')
-   cy.get('.a-button-input').click()*/
-      //cy.get('#quantity_3').click()
-      //cy.get('#a-autoid-0-announce > .a-size-base-plus').click()
-      //cy.get('#a-autoid-1').click()
-      //cy.wait(5000)
-      //cy.get('#a-autoid-8-announce > .a-size-base-plus').click()
-     // cy.wait(5000)
-     // cy.get('#ap_email').type('houssemhajji4@gmail.com')
-     // cy.wait(5000)
-      //cy.get('.a-button-inner > #continue').click()
-     // cy.get('#a-autoid-1').click()
+
+    // Visiter le site Amazon
+    cy.visit('https://www.amazon.com/')
+
+    // Saisir "Acer" dans la barre de recherche et appuyer sur Entrée
+    cy.get('#twotabsearchtextbox').type('Acer{enter}')
+    
+    // Cliquer sur le panier
+    cy.get('#nav-cart').click()
+    
+    // Cliquer sur le bouton de "connection"
+    cy.get('#a-autoid-0-announce').click()
+
+    // Saisir l'adresse email
+    cy.get('#ap_email').type('houssemhajji414@gmail.com')
+    
+    // Cliquer sur le bouton "Continuer"
+    cy.get('.a-button-inner > #continue').click()
+    
+    // Saisir le mot de passe
+    cy.get('#ap_password').type('azerTY')
+
+    //Cliquer sur le bouton de connexion
+    cy.get('#signInSubmit').click()
+
+    // Cliquer pour procéder au paiement
+    cy.get('#sc-buy-box-ptc-button > .a-button-inner > .a-button-input').click()
+    
+    // Ouvrir le menu déroulant pour sélectionner le pays
+    cy.get('#address-ui-widgets-countryCode > .a-button-inner > .a-button-text').click()
+
+    //Sélectionner la Tunisie
+    cy.get('#address-ui-widgets-countryCode-dropdown-nativeId_223').click()
+
+    // Attendre 2 secondes
+    cy.wait(2000)
+
+     // Saisir le nom complet
+      cy.get('#address-ui-widgets-enterAddressFullName').type('Houssem Hajji')
+
+      // Saisir l'adresse ligne 1
+      cy.get('#address-ui-widgets-enterAddressLine1').type('Rue de Carthage')
+
+      // Saisir la ville
+      cy.get('#address-ui-widgets-enterAddressCity').type('Ariana')
+
+      // Saisir le code postal
+      cy.get('#address-ui-widgets-enterAddressPostalCode').type('2081')
+
+      // Saisir le numéro de téléphone
+      cy.get('#address-ui-widgets-enterAddressPhoneNumber').type('21123456')
+
+      // Cocher l'option pour utiliser l'adresse par défaut
+      cy.get('#address-ui-widgets-use-as-my-default').check()
+      
+      // Cliquer pour soumettre le formulaire d'adresse
+      cy.get('#address-ui-widgets-form-submit-button > .a-button-inner > .a-button-input').click()
     })
   
   })
